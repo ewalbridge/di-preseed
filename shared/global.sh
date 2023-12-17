23e0cc5bbfba07235ae53f962d1b44c1f3ee4cad
ewalbridge <ewalbridge@gmail.com>
2023-12-16 21:24:27 -0500

    Added run-once.service

--- shared/global.sh
+++ shared/global.sh
@@ -11,20 +11,30 @@ _var_url_root='https://raw.githubusercontent.com/ewalbridge/di-preseed/main';
 # temp install file location
 _var_install_tmp_path='/root/tmp';
 
+# run once systemd unity name
+_var_run_once_unit_name='run-once.service';
+
+# run once script name
+_var_run_once_script_name='run-once.sh';
+
 # download and start custom install functions
 # this is called from preseed.cfg d-i preseed/late_command
 _fn_bootstrap () {
-    _download_file 'shared' 'run-once.sh';
-    _download_file ${__custom_preseed} 'during-install.sh';
-    _download_file ${__custom_preseed} 'after-install.sh';
+    # /etc/systemd/system
+    _download_file 'shared' '/root/tmp' ${_var_run_once_unit_name};
+    # /usr/local/bin
+    _download_file 'shared' '/root/tmp' ${_var_run_once_script_name};
+    _download_file ${__custom_preseed} ${_var_install_tmp_path} 'during-install.sh';
+    _download_file ${__custom_preseed} ${_var_install_tmp_path} 'after-install.sh';
 }
 
 _download_file () {
     local url_location=${1};
-    local file_name=${2};
+    local save_to_path=${2};
+    local file_name=${3};
     local download_from_url=${_var_url_root}/${url_location}/${file_name};
-    local save_to_path=${_var_install_tmp_path}/${file_name};
-    wget -O ${download_from_url} ${save_to_path};
+    echo wget -O ${download_from_url} ${save_to_path}/${file_name};
+    wget ${download_from_url} -O ${save_to_path}/${file_name};
 }
 
 _fn_bootstrap;
