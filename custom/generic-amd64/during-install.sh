b3887db5068e9868369700e03e8b13d426472d84
ewalbridge <ewalbridge@gmail.com>
2023-12-17 20:02:51 -0500

    moved download and apt install to during install

--- custom/generic-amd64/during-install.sh
+++ custom/generic-amd64/during-install.sh
@@ -10,3 +10,37 @@ var_install_tmp_path='/root/tmp';
 # log name of script
 _fn_logger 'generic-amd64/during-install.sh';
 ### INCLUDE ###
+
+var_core_swapfile_script_name='core-swapfile.sh';
+var_core_dhcpclient_script_name='core-dhcpclient.sh';
+var_core_btrfs_script_name='core-btrfs.sh';
+
+### ADDITIONAL APT PACKAGES ###
+var_apt_install_packages='openssh-server avahi-daemon libnss-mdns augeas-tools tcpdump';
+### ADDITIONAL APT PACKAGES ###
+
+fn_install_custom (){
+    # instead of preseed.cfg: d-i pkgsel/include
+    # offers greater control
+    apt-get install -y ${var_apt_install_packages}
+}
+
+fn_download_files () {
+    # download core-swapfile.sh and set as executable 
+    _fn_download_file ${_var_shared_url_path} ${var_install_tmp_path} ${var_core_swapfile_script_name};
+    chmod +x ${var_install_tmp_path}/${var_core_swapfile_script_name};
+
+    # download core-dhcpclient.sh and set as executable 
+    _fn_download_file ${_var_shared_url_path} ${var_install_tmp_path} ${var_core_dhcpclient_script_name};
+    chmod +x ${var_install_tmp_path}/${var_core_dhcpclient_script_name};
+
+    # download core-btrfs.sh and set as executable 
+    _fn_download_file ${_var_shared_url_path} ${var_install_tmp_path} ${var_core_btrfs_script_name};
+    chmod +x ${var_install_tmp_path}/${var_core_btrfs_script_name};
+}
+
+# download all installer related files first
+fn_download_files;
+
+# install custom packages second
+fn_install_custom;
