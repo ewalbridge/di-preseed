72bbe27b6defcb48d727725769638331a8151afe
ewalbridge <ewalbridge@gmail.com>
2023-12-17 20:23:55 -0500

    moved all to functions generic-amd64/during-install.sh

--- custom/generic-amd64/after-install.sh
+++ custom/generic-amd64/after-install.sh
@@ -10,27 +10,3 @@ var_install_tmp_path='/root/tmp';
 # log name of script
 _fn_logger 'generic-amd64/after-install.sh';
 ### INCLUDE ###
-
-fn_core_swapfile () {
-    ${var_install_tmp_path}/${var_core_swapfile_script_name};
-    _fn_logger 'run core-swapfile.sh';
-}
-
-fn_core_dhcpclient () {
-    ${var_install_tmp_path}/${var_core_dhcpclient_script_name};
-    _fn_logger 'run core-dhcpclient.sh';
-}
-
-fn_core_btrfs () {
-    ${var_install_tmp_path}/${var_core_btrfs_script_name};
-    _fn_logger 'run core-btrfs.sh';
-}
-
-# create swapfile
-fn_core_swapfile;
-
-# replace dhcp client
-fn_core_dhcpclient;
-
-# create btrfs subvolumes last
-fn_core_btrfs;
