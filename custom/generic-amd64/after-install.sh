99f0d37e990cb5b4f17da4d7c4b6cd2948c1782d
ewalbridge <ewalbridge@gmail.com>
2023-12-17 20:34:51 -0500

    moved download and apt to generic-amd64/during-install.sh

--- custom/generic-amd64/after-install.sh
+++ custom/generic-amd64/after-install.sh
@@ -10,3 +10,31 @@ var_install_tmp_path='/root/tmp';
 # log name of script
 _fn_logger 'generic-amd64/after-install.sh';
 ### INCLUDE ###
+
+var_core_swapfile_script_name='core-swapfile.sh';
+var_core_dhcpclient_script_name='core-dhcpclient.sh';
+var_core_btrfs_script_name='core-btrfs.sh';
+
+fn_core_swapfile () {
+    ${var_install_tmp_path}/${var_core_swapfile_script_name};
+    _fn_logger 'run core-swapfile.sh';
+}
+
+fn_core_dhcpclient () {
+    ${var_install_tmp_path}/${var_core_dhcpclient_script_name};
+    _fn_logger 'run core-dhcpclient.sh';
+}
+
+fn_core_btrfs () {
+    ${var_install_tmp_path}/${var_core_btrfs_script_name};
+    _fn_logger 'run core-btrfs.sh';
+}
+
+# create swapfile
+fn_core_swapfile;
+
+# replace dhcp client
+fn_core_dhcpclient;
+
+# create btrfs subvolumes last
+fn_core_btrfs;
--- custom/generic-amd64/during-install.sh
+++ custom/generic-amd64/during-install.sh
@@ -11,13 +11,19 @@ var_install_tmp_path='/root/tmp';
 _fn_logger 'generic-amd64/during-install.sh';
 ### INCLUDE ###
 
+var_core_swapfile_script_name='core-swapfile.sh';
+var_core_dhcpclient_script_name='core-dhcpclient.sh';
+var_core_btrfs_script_name='core-btrfs.sh';
+
 ### ADDITIONAL APT PACKAGES ###
 var_apt_install_packages='openssh-server avahi-daemon libnss-mdns augeas-tools tcpdump';
 ### ADDITIONAL APT PACKAGES ###
 
-var_core_swapfile_script_name='core-swapfile.sh';
-var_core_dhcpclient_script_name='core-dhcpclient.sh';
-var_core_btrfs_script_name='core-btrfs.sh';
+fn_install_custom (){
+    # instead of preseed.cfg: d-i pkgsel/include
+    # offers greater control
+    apt-get install -y ${var_apt_install_packages}
+}
 
 fn_download_files () {
     # download core-swapfile.sh and set as executable 
@@ -33,39 +39,8 @@ fn_download_files () {
     chmod +x ${var_install_tmp_path}/${var_core_btrfs_script_name};
 }
 
-fn_install_custom (){
-    # instead of preseed.cfg: d-i pkgsel/include
-    # offers greater control
-    apt-get install -y ${var_apt_install_packages}
-}
-
-fn_core_swapfile () {
-    ${var_install_tmp_path}/${var_core_swapfile_script_name};
-    _fn_logger 'run core-swapfile.sh';
-}
-
-fn_core_dhcpclient () {
-    ${var_install_tmp_path}/${var_core_dhcpclient_script_name};
-    _fn_logger 'run core-dhcpclient.sh';
-}
-
-fn_core_btrfs () {
-    ${var_install_tmp_path}/${var_core_btrfs_script_name};
-    _fn_logger 'run core-btrfs.sh';
-}
-
-
-# download all installer related files first
-fn_download_files;
-
-# install custom packages second
+# install custom packages
 fn_install_custom;
 
-# create swapfile
-fn_core_swapfile;
-
-# replace dhcp client
-fn_core_dhcpclient;
-
-# create btrfs subvolumes last
-fn_core_btrfs;
+# download all installer related files
+fn_download_files;
